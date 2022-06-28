6448c4c3331d1fab6a58bb3d59eb5d3d0eba753c
Nabeel Imran <nabeel.imran852@gmail.com>
2022-06-28 23:33:21 +0500

    init commit

---
+++ programs/social-blocks/src/lib.rs
@@ -0,0 +1,55 @@
+use anchor_lang::prelude::*;
+
+pub mod bid;
+pub mod mint;
+pub mod sell;
+
+use bid::*;
+use mint::*;
+use sell::*;
+
+declare_id!("2oVAtLipueh6M81ehshSMduQvTBqgFmzLY6Q5rpUv4mG");
+// account verification(uname(immutable), display name, image url)
+// post creation (uri, imgUrl, title, post, owner, creator, type(buyable, bidable, not for sale)) (uer must have account)
+// bid and buy
+// claim bid award
+// like reward claim
+
+#[program]
+pub mod social_blocks {
+  use super::*;
+
+  pub fn mint(
+    ctx: Context<MintNft>,
+    metadata_title: String, 
+    metadata_symbol: String, 
+    metadata_uri: String,
+  ) -> Result<()> {
+      mint::mint(
+        ctx,
+        metadata_title,
+        metadata_symbol,
+        metadata_uri,
+      )
+  }
+
+  pub fn sell(
+    ctx: Context<SellNft>,
+    sale_lamports: u64
+  ) -> Result<()> {
+    sell::sell(
+      ctx,
+      sale_lamports,
+    )
+  }
+
+  // pub fn bid(
+  //   ctx: Context<BidNft>,
+  //   bid_lamports: u64,
+  // ) -> Result<()> {
+  //   bid::bid(
+  //     ctx,
+  //     bid_lamports,
+  //   )
+  // }
+}
